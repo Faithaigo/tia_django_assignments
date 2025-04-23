0382378ec9e15f6d7f8cc22424fac4eb960cf271
Aigo Faith <aigofaith@gmail.com>
2025-04-23 13:54:49 +0300

    Register apps to the admin panel

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    full_name = models.TextField()
+    full_name = models.CharField()
     username = models.CharField(max_length=50)
     email = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=20)
